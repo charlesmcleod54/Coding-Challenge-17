ab34fa0daa593ef89e1cb9f8e422f2b2be5acc99
Charles McLeod <charlesmcleod@usf.edu>
2025-03-30 16:34:33 -0400

    Task 2 - Created a SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -23,3 +23,29 @@ let customer1 = new Customer("John Smith", "sjohn@gmail.com");
 customer1.addPurchase(100);
 customer1.addPurchase(50);
 customer1.getTotalSpent();
+
+// Task 2: Create a SalesRep Class
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    }
+
+    addClient(customer) {
+        this.clients.push(customer);
+        console.log(`${customer.name} added to ${this.name}'s client list.`);
+    }
+
+    getClientTotal(name) {
+        const client = this.clients.find(client => client.name === name);
+        if (client) {
+            return client.getTotalSpent();
+        } else {
+            console.log(`Client ${name} not found.`);
+        }
+    }
+}
+
+let salesRep = new SalesRep("Thomas");
+salesRep.addClient(customer1);
+salesRep.getClientTotal("John Smith");

a31001e05793ee261cebbfff1c3bb014ed65ecb1
Charles McLeod <charlesmcleod@usf.edu>
2025-03-30 16:28:57 -0400

    Task 1 - Created Customer Class

---
+++ cc_17.js
@@ -0,0 +1,25 @@
+// Task 1: Create a Customer Class
+class Customer {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.purchaseHistory = [];
+        console.log(`New customer created: ${name}`);
+    }
+
+    addPurchase(amount) {
+        this.purchaseHistory.push(amount);
+        console.log(`${this.name} made a purchase of $${amount}`);
+    }
+
+    getTotalSpent() {
+        const totalSpent = this.purchaseHistory.reduce((total, amount) => total + amount, 0);
+        console.log(`${this.name}'s total spent: $${totalSpent}`);
+        return totalSpent;
+    }
+}
+
+let customer1 = new Customer("John Smith", "sjohn@gmail.com");
+customer1.addPurchase(100);
+customer1.addPurchase(50);
+customer1.getTotalSpent();
